1251458ded94405ac2b0640a8c6f5e2096b27d22
csculley <cs.sculley@gmail.com>
2017-10-28 13:15:27 -0400

    Button code in place

--- app/src/main/java/co/negitoromaki/fosscarina/Ocarina12HoleFragment.java
+++ app/src/main/java/co/negitoromaki/fosscarina/Ocarina12HoleFragment.java
@@ -3,8 +3,13 @@ package co.negitoromaki.fosscarina;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.KeyEvent;
+import android.app.Activity;
 
 /**
  * Created by csculley on 10/28/17.
@@ -16,6 +21,94 @@ public class Ocarina12HoleFragment extends Fragment {
         if (container != null) {
             container.removeAllViews();
         }
+
+        Button b1 = (Button) container.findViewById(R.id.button1);
+        Button b2 = (Button) container.findViewById(R.id.button2);
+        Button b3 = (Button) container.findViewById(R.id.button3);
+        Button b4 = (Button) container.findViewById(R.id.button4);
+        Button b5 = (Button) container.findViewById(R.id.button5);
+        Button b6 = (Button) container.findViewById(R.id.button6);
+        Button b7 = (Button) container.findViewById(R.id.button7);
+        Button b8 = (Button) container.findViewById(R.id.button8);
+        //Buttons 9 and 10 are volume buttons
+        Button b11 = (Button) container.findViewById(R.id.button11);
+        Button b12 = (Button) container.findViewById(R.id.button12);
+
+
+        TouchListener12 touchListener = new TouchListener12();
+        b1.setOnTouchListener(touchListener);
+        b2.setOnTouchListener(touchListener);
+        b3.setOnTouchListener(touchListener);
+        b4.setOnTouchListener(touchListener);
+        b5.setOnTouchListener(touchListener);
+        b6.setOnTouchListener(touchListener);
+        b7.setOnTouchListener(touchListener);
+        b8.setOnTouchListener(touchListener);
+        //Buttons 9 and 10 are volume buttons
+        b11.setOnTouchListener(touchListener);
+        b12.setOnTouchListener(touchListener);
+
         return inflater.inflate(R.layout.ocarina_12_hole, container, false);
     }
+}
+
+class TouchListener12 implements OnTouchListener {
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch(v.getId()){
+            case 1:
+                //do stuff for button 1
+                break;
+            case 2:
+                //do stuff for button 2
+                break;
+            case 3:
+                //do stuff for button 3
+                break;
+            case 4:
+                //do stuff for button 4
+                break;
+            case 5:
+                //do stuff for button 5
+                break;
+            case 6:
+                //do stuff for button 6
+                break;
+            case 7:
+                //do stuff for button 7
+                break;
+            case 8:
+                //do stuff for button 8
+                break;
+            case 11:
+                //do stuff for button 11
+                break;
+            case 12:
+                //do stuff for button 12
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    //do stuff for button 9
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    //do stuff for button 10
+                }
+                return true;
+            default:
+                return dispatchKeyEvent(event);
+        }
+    }
+
 }
--- app/src/main/java/co/negitoromaki/fosscarina/Ocarina4HoleFragment.java
+++ app/src/main/java/co/negitoromaki/fosscarina/Ocarina4HoleFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.MotionEvent;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 
 /**
  * Created by csculley on 10/28/17.
@@ -16,6 +19,42 @@ public class Ocarina4HoleFragment extends Fragment {
         if (container != null) {
             container.removeAllViews();
         }
+
+        Button b1 = (Button) container.findViewById(R.id.button1);
+        Button b2 = (Button) container.findViewById(R.id.button2);
+        Button b3 = (Button) container.findViewById(R.id.button3);
+        Button b4 = (Button) container.findViewById(R.id.button4);
+
+        TouchListener4 touchListener = new TouchListener4();
+        b1.setOnTouchListener(touchListener);
+        b2.setOnTouchListener(touchListener);
+        b3.setOnTouchListener(touchListener);
+        b4.setOnTouchListener(touchListener);
+
         return inflater.inflate(R.layout.ocarina_4_hole, container, false);
     }
+
+}
+
+class TouchListener4 implements OnTouchListener {
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch(v.getId()){
+            case 1:
+                //do stuff for button 1
+                break;
+            case 2:
+                //do stuff for button 2
+                break;
+            case 3:
+                //do stuff for button 3
+                break;
+            case 4:
+                //do stuff for button 4
+                break;
+        }
+        return true;
+    }
+
 }
