6d4af6961214e2144a1933a57770263871c78bd8
csculley <cs.sculley@gmail.com>
2017-10-29 04:11:41 -0400

    Remove typo

--- app/src/main/java/co/negitoromaki/fosscarina/OcarinaTouchListener.java
+++ app/src/main/java/co/negitoromaki/fosscarina/OcarinaTouchListener.java
@@ -81,7 +81,7 @@ public class OcarinaTouchListener implements View.OnTouchListener {
                 return 0;
         }
     }
-    
+
     public static boolean[] getButtons() {
         return buttons;
     }
